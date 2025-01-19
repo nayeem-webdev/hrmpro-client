3755d2ccc6b6315baff7c6ab9fb1934264b9bb6c
Nayeem Uddin <unayeem33@gmail.com>
2025-01-19 21:09:04 +0600

    Payment issue fixed

--- src/pages/employee-pages/PaymentHistory.jsx
+++ src/pages/employee-pages/PaymentHistory.jsx
@@ -6,15 +6,19 @@ import AuthContext from "../../context/AuthContext";
 
 const PaymentHistory = () => {
   const { user } = useContext(AuthContext);
+  console.log(user);
+  const { uid } = user;
+  console.log(uid);
   const { isLoading, error, data } = useQuery({
-    queryKey: ["users"],
+    queryKey: ["payments"],
     queryFn: async () => {
-      const res = await API.get(`/salaries?uid${user.uid}`);
+      const res = await API.get(`/salaries?uid=${uid}`);
       if (res.data) {
         return res.data;
       }
       throw new Error("Failed to fetch user data");
     },
+    enabled: !!uid,
   });
   return (
     <>
