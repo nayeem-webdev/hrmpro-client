edd5e4b6152be1b18a3c24610df76898c49f5478
Nayeem Uddin <unayeem33@gmail.com>
2025-02-19 12:51:26 +0600

    Dashboard

--- src/pages/shared_pages/DashboardStats.jsx
+++ src/pages/shared_pages/DashboardStats.jsx
@@ -1,37 +1,46 @@
-import {
-  Bar,
-  BarChart,
-  CartesianGrid,
-  ResponsiveContainer,
-  XAxis,
-  YAxis,
-} from "recharts";
-import { Tooltip } from "react-tooltip";
-import ProgressInsights from "../../components/dashboard/ProgressInsights";
+// import {
+//   Bar,
+//   BarChart,
+//   CartesianGrid,
+//   ResponsiveContainer,
+//   XAxis,
+//   YAxis,
+// } from "recharts";
+// import { Tooltip } from "react-tooltip";
+// import ProgressInsights from "../../components/dashboard/ProgressInsights";
+// import { FaChartPie, FaCog, FaUsers } from "react-icons/fa";
 
 const DashboardStats = () => {
   // Fake data for charts
-  const earningsData = [
-    { month: "January", earnings: 4000 },
-    { month: "February", earnings: 3500 },
-    { month: "March", earnings: 5000 },
-    { month: "April", earnings: 4700 },
-    { month: "May", earnings: 5300 },
-    { month: "June", earnings: 4800 },
-  ];
+  // const earningsData = [
+  //   { month: "January", earnings: 4000 },
+  //   { month: "February", earnings: 3500 },
+  //   { month: "March", earnings: 5000 },
+  //   { month: "April", earnings: 4700 },
+  //   { month: "May", earnings: 5300 },
+  //   { month: "June", earnings: 4800 },
+  // ];
 
-  const ordersData = [
-    { month: "January", orders: 50 },
-    { month: "February", orders: 40 },
-    { month: "March", orders: 60 },
-    { month: "April", orders: 70 },
-    { month: "May", orders: 75 },
-    { month: "June", orders: 65 },
-  ];
+  // const ordersData = [
+  //   { month: "January", orders: 50 },
+  //   { month: "February", orders: 40 },
+  //   { month: "March", orders: 60 },
+  //   { month: "April", orders: 70 },
+  //   { month: "May", orders: 75 },
+  //   { month: "June", orders: 65 },
+  // ];
 
   return (
     <>
-      <ProgressInsights />
+      <div className="bg-gradient-to-r from-blue-500 to-primary h-full flex items-center justify-center flex-col text-white p-8 rounded-2xl shadow-md text-center">
+        <h1 className="text-4xl md:text-6xl font-bold">
+          Welcome to Your Dashboard 🎉
+        </h1>
+        <p className="mt-2 md:mt-6 text-lg md:text-2xl">
+          Manage everything with ease and efficiency.
+        </p>
+      </div>
+      {/* <ProgressInsights />
       <div className="flex flex-col gap-6 lg:flex-row">
         <div className="w-full">
           <h4 className="text-lg font-bold text-gray-800 mb-4">
@@ -66,7 +75,7 @@ const DashboardStats = () => {
             </ResponsiveContainer>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
